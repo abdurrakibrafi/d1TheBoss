36ca417e23424c417294d901236262358f227095
Mamungithube <orroshidmdmamun50@gmail.com>
2026-06-13 17:45:35 +0600

    change name

--- apps/homepage/urls.py
+++ apps/homepage/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('daily-verse/', views.DailyVerseView.as_view(), name='daily-verse'),
     # path('refresh-daily-verse/', views.RefreshDailyVerseView.as_view(), name='refresh-daily-verse'),
-    path('refresh-daily-verse/', views.JourneyDailyVerseView.as_view(), name='journey-verse'),
+    path('refresh-daily-verse/', views.JourneyDailyVerseView.as_view(), name='refresh-daily-verse'),
 ]
