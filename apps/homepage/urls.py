21ab5055ee450dc7d04012eb51a31544f3ec5fbe
Mamungithube <orroshidmdmamun50@gmail.com>
2026-06-13 18:01:19 +0600

    -

--- apps/homepage/urls.py
+++ apps/homepage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('daily-verse/', views.DailyVerseView.as_view(), name='daily-verse'),
-    # path('refresh-daily-verse/', views.RefreshDailyVerseView.as_view(), name='refresh-daily-verse'),
-    path('refresh-daily-verse/', views.JourneyDailyVerseView.as_view(), name='refresh-daily-verse'),
+    # path('daily-verse/', views.DailyVerseView.as_view(), name='daily-verse'),
+    path('refresh-daily-verse/', views.RefreshDailyVerseView.as_view(), name='refresh-daily-verse'),
+    path('daily-verse/', views.JourneyDailyVerseView.as_view(), name='daily-verse'),
 ]
